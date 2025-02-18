db9a8e0d3cf383e566945d5fbd7489d6b8ee591e
faridullah77 <faridullahkhan96@gmail.com>
2025-02-18 16:39:17 +0500

    test.app.py file added

---
+++ backend/test_app.py
@@ -0,0 +1,13 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_get_stats(client):
+    response = client.get('/api/stats')
+    assert response.status_code == 200
+    assert b'live_scores' in response.data
+    assert b'top_players' in response.data
